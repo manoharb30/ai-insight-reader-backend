38d779b03f7e2b7a60a5d6940cd10c7015f0174a
manohar <manohar.hello@gmail.com>
2025-04-14 00:56:31 +0530

    added main file

---
+++ src/main/java/com/aiinsightreader/AiInsightReaderApplication.java
@@ -0,0 +1,11 @@
+package com.aiinsightreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInsightReaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiInsightReaderApplication.class, args);
+    }
+}
